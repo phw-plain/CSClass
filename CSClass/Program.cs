11e4a1a2637ae91151350d83cd8d9a69692c21a7
plainphw <s2028@e-mirim.hs.kr>
2022-05-30 14:55:11 +0900

    #2 Math 클래스 메서드 사용법

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -26,6 +26,30 @@ namespace CSClass
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble() * 10);
 
+            /* 
+            List<int> list = new List<int>();
+            list.Add(11);
+            list.Add(22);
+            list.Add(33);
+            list.Add(44);
+            */
+
+            List<int> list = new List<int>() { 52, 273, 32, 42 };
+            list.Remove(42);
+            list.RemoveAt(0);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\t" + item);
+            }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Round(52.273));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.PI);
         }
     }
 }
