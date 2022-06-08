13174ec9cb41437788d5fb8a794ce7d636c8e0a7
plainphw <s2028@e-mirim.hs.kr>
2022-06-08 09:38:38 +0900

    #12 싱글톤패턴 생성자가 private를 쓰는 경우 중 한 케이스

---
+++ CSClass/SingleTon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass
+{
+    internal class SingleTon
+    {
+        private static SingleTon instance = null;
+
+        private SingleTon()
+        {
+
+        }
+        public static SingleTon getInstance()
+        {
+            if(SingleTon.instance == null)
+            {
+                instance = new SingleTon();
+            }
+            return instance;
+        }
+    }
+}
