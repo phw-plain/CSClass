addd407b0d442c82e431e59e53b0b0961a19ff0b
plainphw <s2028@e-mirim.hs.kr>
2022-06-15 10:11:09 +0900

    #18 23-2. 개와 고양이는 동물이다.

---
+++ CSClass/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass
+{
+    public class Animal
+    {
+        int Age;
+
+        public Animal()
+        {
+            Age = 0;
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("냠냠");
+        }
+    }
+}
---
+++ CSClass/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
+}
