55784279f60dea21bcb36f0f3523ff729fbe2d15
plainphw <s2028@e-mirim.hs.kr>
2022-06-28 10:54:38 +0900

    #19 #20 is키워드

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -166,6 +166,36 @@ namespace CSClass
             Console.WriteLine(Fibonacci.Get(50));
             Console.WriteLine(Fibonacci.Get(100));
 
+            /*List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };*/
+            List<Animal> Animals = new List<Animal>() 
+            { 
+                new Dog(), new Dog(), new Dog(), 
+                new Cat(), new Cat(), new Cat() 
+            };
+
+            /*foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }*/
+            foreach (var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+                if(item is Dog) ((Dog)item).Bark();
+                if(item is Cat) ((Cat)item).Meow();
+                // item is Animal
+
+            }
+
         }
     }
 }
